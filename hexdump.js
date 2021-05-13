f10386dd5914f8b37d67230aa125a413df74c7c3
Rhinozz <68926744+Rhinozz@users.noreply.github.com>
2021-05-13 02:39:11 -0400

    Add files via upload

---
+++ hexdump.js
@@ -0,0 +1,22 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: laptop-code;
+
+// Hex Dump (mainly focused on inages) by Rhinozz
+// Very slow for larger images.
+
+// enter file path after "Documents/" - in the files app, it's starting from /iCloud Drive/Scriptable/.
+var bytes = FileManager.iCloud().read("/private/var/mobile/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/").getBytes();
+var hexstack = [];
+var fullhex = [];
+var counter = 0;
+for(const i in bytes) {
+    hexstack.push(("0" + (bytes[i].toString(16))).slice(-2).toUpperCase());
+    counter++;
+    if(counter == 8) {
+        counter = 0;
+        fullhex.push(hexstack.join(" "));
+        hexstack = [];
+    }
+}
+console.log(fullhex.join("\n"));
